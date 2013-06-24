bd55d99b042eaac340be2730f8033a90949ae4ed
Ratchasak Buranapanichkit <bratchasak@eng.psu.ac.th>
2013-06-24 15:21:10 +0700

    complete login

--- app.js
+++ app.js
@@ -4,8 +4,6 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
   , util = require('util')
@@ -14,12 +12,15 @@ var express = require('express')
   , TwitterStrategy = require('passport-twitter').Strategy
   , GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-var FACEBOOK_APP_ID = "484741244941247"
+var FACEBOOK_APP_ID = "484741244941247";
 var FACEBOOK_APP_SECRET = "bad256eab00c4ed73b0a296dfa2eeddc";
+var FACEBOOK_CALLBACK = "http://localhost:3000/auth/facebook/callback";
 var TWITTER_CONSUMER_KEY = "QPfF5l7P9VefiZDFcpSCow";
 var TWITTER_CONSUMER_SECRET = "cn7t91hGQ78ldeGRSE8h6e9k9NSbfkJ0K0HNUeBm4";
+var TWITTER_CALLBACK = "http://127.0.0.1:3000/auth/twitter/callback";
 var GOOGLE_CLIENT_ID = "312966742093.apps.googleusercontent.com";
 var GOOGLE_CLIENT_SECRET = "PzfvfuCEt91fWlKyzG030UjV";
+var GOOGLE_CALLBACK = "http://127.0.0.1:3000/auth/google/callback";
 
 // Passport session setup.
 //   To support persistent login sessions, Passport needs to be able to
@@ -43,12 +44,11 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new FacebookStrategy({
     clientID: FACEBOOK_APP_ID,
     clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
+    callbackURL: FACEBOOK_CALLBACK
   },
   function(accessToken, refreshToken, profile, done) {
     // asynchronous verification, for effect...
     process.nextTick(function () {
-      
       // To keep the example simple, the user's Facebook profile is returned to
       // represent the logged-in user.  In a typical application, you would want
       // to associate the Facebook account with a user record in your database,
@@ -64,12 +64,11 @@ passport.use(new FacebookStrategy({
 passport.use(new TwitterStrategy({
     consumerKey: TWITTER_CONSUMER_KEY,
     consumerSecret: TWITTER_CONSUMER_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+    callbackURL: TWITTER_CALLBACK
   },
   function(token, tokenSecret, profile, done) {
     // asynchronous verification, for effect...
     process.nextTick(function () {
-      
       // To keep the example simple, the user's Twitter profile is returned to
       // represent the logged-in user.  In a typical application, you would want
       // to associate the Twitter account with a user record in your database,
@@ -85,12 +84,11 @@ passport.use(new TwitterStrategy({
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/google/callback"
+    callbackURL: GOOGLE_CALLBACK
   },
   function(accessToken, refreshToken, profile, done) {
     // asynchronous verification, for effect...
     process.nextTick(function () {
-      
       // To keep the example simple, the user's Google profile is returned to
       // represent the logged-in user.  In a typical application, you would want
       // to associate the Google account with a user record in your database,
@@ -105,19 +103,19 @@ var app = express();
 // all environments
 app.configure(function() {
 	app.set('port', process.env.PORT || 3000);
-  	app.set('views', __dirname + '/views');
-  	app.set('view engine', 'jade');
-  	app.use(express.logger());
-  	app.use(express.cookieParser());
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-  	app.use(express.session({ secret: 'keyboard cat' }));
-  	// Initialize Passport!  Also use passport.session() middleware, to support
-  	// persistent login sessions (recommended).
-  	app.use(passport.initialize());
-  	app.use(passport.session());
-  	app.use(app.router);
-  	app.use(express.static(path.join(__dirname, 'public')));
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.use(express.logger());
+	app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+	app.use(express.session({ secret: 'keyboard cat' }));
+	// Initialize Passport!  Also use passport.session() middleware, to support
+	// persistent login sessions (recommended).
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
 });
 
 // development only
@@ -125,14 +123,16 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
+});
 
 app.get('/login', function(req, res){
   res.render('login', { user: req.user });
 });
 
 app.get('/facebook-login', passport.authenticate('facebook'));
- 
+
 app.get('/facebook-token', passport.authenticate('forcedotcom', { failureRedirect: '/error' }),
   function(req, res){
     res.send('Logged In.');
@@ -153,61 +153,84 @@ app.get('/auth/facebook',
   function(req, res){
     // The request will be redirected to Facebook for authentication, so this
     // function will not be called.
-  });
+});
 
 // GET /auth/facebook/callback
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
-app.get('/auth/facebook/callback', 
+app.get('/auth/facebook/callback',
   passport.authenticate('facebook', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
-  });
- 
+});
+
 app.get('/twitter-login', passport.authenticate('twitter'));
- 
+
 app.get('/twitter-token', passport.authenticate('twitter', { failureRedirect: '/error' }),
   function(req, res){
     res.send('Logged In.');
-  });
- 
-  app.get('/google-login', passport.authenticate('google'));
- 
+});
+
+// GET /auth/twitter
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Twitter authentication will involve redirecting
+//   the user to twitter.com.  After authorization, the Twitter will redirect
+//   the user back to this application at /auth/twitter/callback
+app.get('/auth/twitter',
+  passport.authenticate('twitter'),
+  function(req, res){
+    // The request will be redirected to Twitter for authentication, so this
+    // function will not be called.
+});
+
+// GET /auth/twitter/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+});
+
+app.get('/google-login', passport.authenticate('google'));
+
 app.get('/google-token', passport.authenticate('google', { failureRedirect: '/error' }),
   function(req, res){
     res.send('Logged In.');
+});
+
+// GET /auth/google
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Google authentication will involve
+//   redirecting the user to google.com.  After authorization, Google
+//   will redirect the user back to this application at /auth/google/callback
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.profile',
+                                            'https://www.googleapis.com/auth/userinfo.email'] }),
+  function(req, res){
+    // The request will be redirected to Google for authentication, so this
+    // function will not be called.
   });
 
-
-
-app.get('/error', function(req, res){
-  res.send('An error has occured.');
-  });
- 
-app.all('/:label/:mode/*',
-  ensureAuthenticated,
+// GET /auth/google/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
-    console.log(req.session);
-    if(req.session["passport"]["user"] && req.params.label == "fdc") {
-      var restOptions = {
-        useHTTPS : true,
-        host : req.session["passport"]["user"].instance_url,
-        headers: {
-            'Authorization': 'OAuth '+req.session["passport"]["user"].access_token,
-            'Accept':'application/jsonrequest',
-            'Cache-Control':'no-cache,no-store,must-revalidate'
-          }
-      }
- 
-      restProxy.proxy(req,res);
-    }
-  });
- 
-app.get('/*',function(req, res) {
-  res.render(req.url.substring(1,req.url.length)); //really?
-})
+    res.redirect('/');
+});
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
@@ -220,5 +243,5 @@ http.createServer(app).listen(app.get('port'), function(){
 //   login page.
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
+  res.redirect('/login');
 }
